aff5b1888f24eab167184d58b981877867e7a66f
sjwopi <iteterk@gmail.com>
2024-06-03 17:07:24 +0300

    feat lk

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const profileSidebar = document.getElementById('profile-sidebar')
+const changePasswordBtn = document.getElementById('changePassword')
+const passwordModal = document.getElementById('password-modal')
+const passwordModalOverlay = document.getElementById('password-modal__overlay')
+const passwordModalClose = document.getElementById('password-modal__close')
 
 profileSidebar.addEventListener('mouseover', () => {
   profileSidebar.classList.add('open')
 })
 profileSidebar.addEventListener('mouseout', () => {
   profileSidebar.classList.remove('open')
+})
+
+changePasswordBtn.addEventListener('click', () => {
+  passwordModal.classList.add('open')
+})
+passwordModalOverlay.addEventListener('click', () => {
+  passwordModal.classList.remove('open')
+})
+passwordModalClose.addEventListener('click', () => {
+  passwordModal.classList.remove('open')
 })
