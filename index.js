eceeb321f06bad5c90f3ea255641627fd78b308d
sjwopi <iteterk@gmail.com>
2024-06-04 13:04:46 +0300

    feat feedback page mobile

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 window.onload = () => {
   const profileSidebar = document.getElementById('profile-sidebar')
+
   const changePasswordBtn = document.getElementById('changePassword')
   const passwordModal = document.getElementById('password-modal')
   const passwordModalOverlay = document.getElementById('password-modal__overlay')
   const passwordModalClose = document.getElementById('password-modal__close')
+
+  const feedbackSave = document.getElementById('feedbackSave')
+  const feedbackModal = document.getElementById('feedback-modal')
+  const feedbackModalOverlay = document.getElementById('feedback-modal__overlay')
+  const feedbackModalClose = document.getElementById('feedback-modal__close')
+
   const headerModileHamb = document.getElementById('profile-header-mobile__hambbtn')
   const headerModal = document.getElementById('header-modal')
 
@@ -14,7 +21,7 @@ window.onload = () => {
     profileSidebar.classList.remove('open')
   })
 
-  if (changePasswordBtn && passwordModalOverlay && passwordModalClose) {
+  if (changePasswordBtn && passwordModal && passwordModalOverlay && passwordModalClose) {
     changePasswordBtn.addEventListener('click', () => {
       passwordModal.classList.add('open')
     })
@@ -23,6 +30,18 @@ window.onload = () => {
     })
     passwordModalClose.addEventListener('click', () => {
       passwordModal.classList.remove('open')
+    })
+  }
+  if (feedbackSave && feedbackModal && feedbackModalOverlay && feedbackModalClose) {
+    feedbackSave.addEventListener('click', (e) => {
+      e.preventDefault()
+      feedbackModal.classList.add('open')
+    })
+    feedbackModalOverlay.addEventListener('click', () => {
+      feedbackModal.classList.remove('open')
+    })
+    feedbackModalClose.addEventListener('click', () => {
+      feedbackModal.classList.remove('open')
     })
   }
 
