3395780cb1a07886068fa994be5cb54bd184d277
ivan.teteru <ivan.teteruk@sitenn.pro>
2024-06-28 15:19:52 +0300

    fix modal and add cars and drivers page

--- index.js
+++ index.js
@@ -1,63 +1,84 @@
 window.onload = () => {
+  // Открытие сайдбара
   const profileSidebar = document.getElementById('profile-sidebar')
-
-  const changePasswordBtn = document.getElementById('changePassword')
-  const passwordModal = document.getElementById('password-modal')
-  const passwordModalOverlay = document.getElementById('password-modal__overlay')
-  const passwordModalClose = document.getElementById('password-modal__close')
-
-  const feedbackSave = document.getElementById('feedbackSave')
-  const feedbackModal = document.getElementById('feedback-modal')
-  const feedbackModalOverlay = document.getElementById('feedback-modal__overlay')
-  const feedbackModalClose = document.getElementById('feedback-modal__close')
-
-  const headerModileHamb = document.getElementById('profile-header-mobile__hambbtn')
-  const headerModal = document.getElementById('header-modal')
-
-  const docRequest = document.getElementById('doc-request')
-  const docRequestModal = document.getElementById('doc-request-modal')
-  const docRequestOverlay = document.getElementById('doc-request-overlay')
-  const docRequestClose = document.getElementById('doc-request-close')
-
-  const orderOpenBtn = document.querySelectorAll('.order__btn')
-
-
   profileSidebar.addEventListener('mouseover', () => {
     profileSidebar.classList.add('open')
   })
+
   profileSidebar.addEventListener('mouseout', () => {
     profileSidebar.classList.remove('open')
   })
 
-  if (changePasswordBtn && passwordModal && passwordModalOverlay && passwordModalClose) {
-    changePasswordBtn.addEventListener('click', () => {
-      passwordModal.classList.add('open')
-    })
-    passwordModalOverlay.addEventListener('click', () => {
-      passwordModal.classList.remove('open')
-    })
-    passwordModalClose.addEventListener('click', () => {
-      passwordModal.classList.remove('open')
-    })
-  }
-  if (feedbackSave && feedbackModal && feedbackModalOverlay && feedbackModalClose) {
-    feedbackSave.addEventListener('click', (e) => {
-      e.preventDefault()
-      feedbackModal.classList.add('open')
-    })
-    feedbackModalOverlay.addEventListener('click', () => {
-      feedbackModal.classList.remove('open')
-    })
-    feedbackModalClose.addEventListener('click', () => {
-      feedbackModal.classList.remove('open')
-    })
-  }
+  // Открытия шапки на мобилке
+  const headerModileHamb = document.getElementById('profile-header-mobile__hambbtn')
+  const headerModal = document.getElementById('header-modal')
 
   headerModileHamb.addEventListener('click', () => {
     headerModileHamb.classList.toggle('open')
     headerModal.classList.toggle('open')
   })
 
+
+  const modalSupportFunction = (modalOpenBtn, modal, modalOvelay, modalCloseBtn) => {
+    if (modalOpenBtn && modal && modalOvelay && modalCloseBtn) {
+      modalOpenBtn.addEventListener('click', () => {
+        modal.classList.add('open')
+      })
+      modalOvelay.addEventListener('click', () => {
+        modal.classList.remove('open')
+      })
+      modalCloseBtn.addEventListener('click', () => {
+        modal.classList.remove('open')
+      })
+    }
+  }
+
+  // Смена пароля
+  const changePasswordBtn = document.getElementById('changePassword')
+  const passwordModal = document.getElementById('password-modal')
+  const passwordModalOverlay = document.getElementById('password-modal__overlay')
+  const passwordModalClose = document.getElementById('password-modal__close')
+  modalSupportFunction(changePasswordBtn, passwordModal, passwordModalOverlay, passwordModalClose);
+
+  // Отправка фидбека
+  const feedbackSave = document.getElementById('feedbackSave')
+  const feedbackModal = document.getElementById('feedback-modal')
+  const feedbackModalOverlay = document.getElementById('feedback-modal__overlay')
+  const feedbackModalClose = document.getElementById('feedback-modal__close')
+  modalSupportFunction(feedbackSave, feedbackModal, feedbackModalOverlay, feedbackModalClose);
+
+  // Отправка документа
+  const docRequest = document.getElementById('doc-request')
+  const docRequestModal = document.getElementById('doc-request-modal')
+  const docRequestOverlay = document.getElementById('doc-request-overlay')
+  const docRequestClose = document.getElementById('doc-request-close')
+  modalSupportFunction(docRequest, docRequestModal, docRequestOverlay, docRequestClose);
+
+  // Добавление авто
+  const carsAdd = document.getElementById('cars-add')
+  const carsAddModal = document.getElementById('cars-add-modal')
+  const carsAddOverlay = document.getElementById('cars-add-overlay')
+  const carsAddClose = document.getElementById('cars-add-close')
+  modalSupportFunction(carsAdd, carsAddModal, carsAddOverlay, carsAddClose);
+
+  // Добавление водителя
+  const driversAdd = document.getElementById('drivers-add')
+  const driversAddModal = document.getElementById('drivers-add-modal')
+  const driversAddOverlay = document.getElementById('drivers-add-overlay')
+  const driversAddClose = document.getElementById('drivers-add-close')
+  modalSupportFunction(driversAdd, driversAddModal, driversAddOverlay, driversAddClose);
+
+  // Редактирование водителя
+  const driversEdit = document.querySelectorAll('.drivers-change')
+  const driversEditModal = document.getElementById('drivers-change-modal')
+  const driversEditOverlay = document.getElementById('drivers-change-overlay')
+  const driversEditClose = document.getElementById('drivers-change-close')
+  driversEdit.forEach(item => {
+    modalSupportFunction(item, driversEditModal, driversEditOverlay, driversEditClose);
+  })
+
+  // Раскрытие заказа
+  const orderOpenBtn = document.querySelectorAll('.order__btn')
   orderOpenBtn.forEach(item => {
     item.addEventListener('click', (e) => {
       e.target.classList.toggle("open");
@@ -65,15 +86,12 @@ window.onload = () => {
       e.target.parentElement.querySelector('.order__container').classList.toggle("open");
     })
   })
-  if (docRequest && docRequestModal && docRequestOverlay && docRequestClose) {
-    docRequest.addEventListener('click', () => {
-      docRequestModal.classList.add('open')
+
+  // Раскрытие водитея 
+  const driverOpenBtn = document.querySelectorAll('.driver__open-btn')
+  driverOpenBtn.forEach(item => {
+    item.addEventListener('click', (e) => {
+      e.target.parentElement.classList.toggle("open");
     })
-    docRequestOverlay.addEventListener('click', () => {
-      docRequestModal.classList.remove('open')
-    })
-    docRequestClose.addEventListener('click', () => {
-      docRequestModal.classList.remove('open')
-    })
-  }
+  })
 }
